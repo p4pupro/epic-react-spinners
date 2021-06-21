eb77790592370067285636f2cdfa9efa054f0eda
p4pupro <domingoperezguerrero@gmail.com>
2021-06-21 14:31:24 +0100

    preparing to npm

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+declare module 'epic-react-spinners' {
+    import { Component } from 'react'
+  
+    const Atom: Component
+    const Orbit: Component
+    const Trinity: Component
+    const Electron: Component
+    const Square: Component
+    const SelfSquare: Component
+    const Rhombuses: Component
+    const Flower: Component
+    const HalfCircle: Component
+    const FingerPrint: Component
+  
+    export {
+        Atom, Orbit, Trinity, Electron, Square, SelfSquare, Rhombuses, Flower, HalfCircle, FingerPrint
+    }
+  }
